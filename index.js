8c68270178a8d26ed4e1226c606e0e495886ee3e
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-08-31 22:38:31 +0300

    11 routing node js

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const http = require('http');
 
 const server = http.createServer(function(req, res) {
   console.log("URL сторінки: " + req.url);
-  // res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'}); // вказуємо text/html
-  res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'}); // щоб виводити json
-  /*const myReadShort = fs.createReadStream(__dirname + '/index.html', 'utf-8');
-  myReadShort.pipe(res); // виводимо контент поступово*/
-  let obj = {
-    model: 'Audi',
-    speed: '234.5',
-    wheels: 4
-  };
-  res.end(JSON.stringify(obj)) // виведемо об'єкт з json на сторінку
+  if (req.url === '/index' || req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+  } else if (req.url === '/contacts') {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    fs.createReadStream(__dirname + '/contacts.html').pipe(res);
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
+    fs.createReadStream(__dirname + '/404.html').pipe(res);
+  }
 });
 server.listen(3000, '127.0.0.1');
 console.log("Ми відслідковуємо порт 3000");
