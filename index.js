b062947cf234f6ae453b59c32c1f8a2656c5fdcf
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-08-31 14:44:57 +0300

    08 work and creation server

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
+//link module http
+const http = require('http');
+// в цю змінну ми помістимо створення нашого серверу
+const server = http.createServer(function(req, res) {
+  console.log("URL сторінки: " + req.url);
+  //в функції будуть дані
+  // Заголовки / також треба вказувати кодування
+  res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'}); // після перезагрузки серверу побачимо результат
+  //завершуємо отримання данних і виводимо інфу
+  res.end('Привіт світ!');
+}); // створили сервер
 
-const fs = require('fs');
-// асинхронне видалення файлу / другим параметром передамо функцію, яка буде спрацьовувати після видалення файлу file.txt
-//fs.unlink('file.txt', function() {}); // файл видалено
-// якщо не застосовувати function() {}, буде попередження
+server.listen(3000, '127.0.0.1');
+console.log("Ми відсліджуємо порт 3000"); // http://127.0.0.1:3000/ - за цією адресою буде наш сервер
 
-// Будемо створювати директорії і видаляти
-// спочатку створюємо директорії синхронно
-//fs.mkdirSync('new-one'); // з'явиться папка new-one
-
-//Створими папку асинхронно і файла
-/*
-fs.mkdir('new-one', function() {
-  fs.writeFile('./new-one/some_new.txt', 'Передаємо строку другим параметром!', function() {
-    console.log('All good!');
-  });
-});
-*/
-
-//Тепер видалимо її асинхронно
-// видаляэмо файли в папці
-fs.unlink('./new-one/some_new.txt', function() {
-  console.log('Deleted file!');
-});
-// після чого видаляємо саму папку
-fs.rmdir('new-one', function() {
-  console.log('Deleted dir!');
-});
 
 
