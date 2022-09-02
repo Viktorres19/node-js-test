fae128f0ae4bb4ca9f6c20df8f2a8c1041b31893
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-09-02 09:28:06 +0300

    16-html-forms

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 // визиваємо експрес і присвоюємо його до змінної
 const app = express();
-
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+//вказуємо який енджин будемо використовувати
 app.set('view engine', 'ejs');
+
+app.use('/public', express.static('public'));
 
 // щоб відображати файли в браузері треба змінити метод send на sendFile
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+  // res.sendFile(__dirname + "/index.html");
+  res.render('index')
 });
-app.get('/news', function(req, res) {
-  res.sendFile(__dirname + "/contacts.html");
+// скористаємось bodyParser
+app.post('/about', urlencodedParser, function(req, res) {
+  if (!req.body) return res.sendStatus(400);
+  console.log(req.body);
+  // буде рендеритись ця сторінка
+  res.render('about-success', {data: req.body});
+});
+app.get('/about', function(req, res) {
+  // res.sendFile(__dirname + "/about.html");
+  res.render('about')
 });
 //тепер з параметрами які будуть мінятись
 /*app.get('/contacts/:id', function(req, res) {
@@ -17,8 +30,10 @@ app.get('/news', function(req, res) {
 })*/
 //тепер використаємо шаблон
 app.get('/news/:id', function(req, res) {
-  const obj = {title: "Новина", id: 4, paragraphs: ['Параграф', 'Звичайний текст', 'Числа: 2, 4, 5', 99]};
+  const obj = {title: "Новина", id: 4, paragraphs: ['Paragraph', 'Usual text', 'Numbers: 2, 4, 5', 99]};
+  // шаблон news ejs
   res.render('news', {newsId: req.params.id, newParam: 234, obj: obj});
+  // http://127.0.0.1:3000/news/%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D1%8F
 });
 // слухаємо порт
 app.listen(3000);
@@ -36,7 +51,7 @@ const server = http.createServer(function(req, res) {
     fs.createReadStream(__dirname + '/index.html').pipe(res);
   } else if (req.url === '/contacts') {
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    fs.createReadStream(__dirname + '/contacts.html').pipe(res);
+    fs.createReadStream(__dirname + '/news.html').pipe(res);
   } else {
     res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
     fs.createReadStream(__dirname + '/404.html').pipe(res);
